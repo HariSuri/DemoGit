52f17e82b4c9726ed5705f4653b6a96c324b9ce2
HariSuri <52205615+HariSuri@users.noreply.github.com>
2024-07-01 15:57:32 +0530

    removed line in master

--- src/test/java/Practice/demo/Git.java
+++ src/test/java/Practice/demo/Git.java
@@ -5,9 +5,7 @@ public class Git {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("First comment");
-		System.out.println("Second comment");
-		System.out.println("third comment");
-		
+		System.out.println("Second comment");		
 
 }
 }
